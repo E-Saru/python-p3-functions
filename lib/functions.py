1c6df3b8311fa6c83bd783a569731fadfd7e2344
E.Saru <efjeniah.mwawughanga@student.moringaschool.com>
2024-02-29 05:17:21 +0300

    Done

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,35 @@
 #!/usr/bin/env python3
 
 def greet_programmer():
-    pass
+    print("Hello, programmer!")
+
+greet_programmer()
+    # pass
 
 def greet(name):
-    pass
+    print(f"Hello, {name}!")
+    
+greet("Efjeniah")
+    # pass
 
 def greet_with_default(name="programmer"):
-    pass
+    print(f"Hello, {name}!")
+
+greet_with_default("Efjeniah")
+greet_with_default()
+    # pass
 
 def add(num1, num2):
-    pass
+    return num1+num2
+    # pass
 
 def halve(number):
-    pass
+    return number/2
+    # pass
+
+# def my_function(param):
+#     print("Running my_function")
+#     return param + 1
+
+# my_function_return_value = my_function(1)
+# my_function_return_value
